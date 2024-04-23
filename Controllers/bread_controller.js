713129351a11657a4a75a99d4cddcb0d37848064
Karon Oquendo <Keoquendo0308@gmail.com>
2024-04-22 20:18:35 -0400

    sending index and show data

--- Controllers/bread_controller.js
+++ Controllers/bread_controller.js
@@ -2,14 +2,28 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('index', {
+    breads: Bread
+  })
+// res.send(Bread)
 })
+
+// SHOW
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
 })
+
+
 
 
 module.exports = breads
